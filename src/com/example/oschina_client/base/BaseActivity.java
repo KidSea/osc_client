67dbdc7495ed6cee1efd5f85ccd9e4021af3bc8e
KidSea <594643219@qq.com>
2016-10-25 23:28:46 +0800

    commit 7

--- src/com/example/oschina_client/base/BaseActivity.java
+++ src/com/example/oschina_client/base/BaseActivity.java
@@ -1,59 +1,274 @@
 package com.example.oschina_client.base;
 
-import com.example.oschina_client.AppManager;
-import com.example.oschina_client.interf.BaseViewInterface;
+import org.kymjs.kjframe.utils.StringUtils;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.LayoutParams;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Spinner;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+
+import com.example.oschina_client.AppManager;
+import com.example.oschina_client.R;
+import com.example.oschina_client.dialog.CommonToast;
+import com.example.oschina_client.interf.BaseViewInterface;
+import com.example.oschina_client.utils.TDevice;
+
 /**
  * BaseActionBar Activity基类
+ * 
  * @author yuxuehai
- *
+ * 
  */
-public class BaseActivity extends ActionBarActivity implements BaseViewInterface, OnClickListener{
+public class BaseActivity extends ActionBarActivity implements
+		BaseViewInterface, OnClickListener {
+	public static final String INTENT_ACTION_EXIT_APP = "INTENT_ACTION_EXIT_APP";
 
+	private boolean _isVisible;
+
+	protected LayoutInflater mInflater;
+	protected ActionBar mActionBar;
+	private TextView mTvActionTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		AppManager.getAppManager().addActivity(this);
-		if(!hasActionBar()){
-			
+		if (!hasActionBar()) {
+
+		}
+		onBeforeSetContentLayout();
+		if (getLayoutID() != 0) {
+			setContentView(getLayoutID());
+		}
+		mActionBar = getSupportActionBar();
+		mInflater = getLayoutInflater();
+
+		if (hasActionBar()) {
+			initActionBar(mActionBar);
+		}
+
+		// 通过注解绑定控件
+		ButterKnife.inject(this);
+
+		init(savedInstanceState);
+		initView();
+		initData();
+		_isVisible = true;
+
+	}
+
+	protected void init(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void initActionBar(ActionBar actionBar) {
+		// TODO Auto-generated method stub
+		if (actionBar == null) {
+			return;
+		}
+		if (hasBackButton()) {
+			// 让ActionBar自定义内容
+			mActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+			int layoutRes = getAtionBarCustomView();
+
+			// ------------------------------------------------- 创建自定义布局 ↓
+			View view = inflateView(layoutRes == 0 ? R.layout.actionbar_custom_backtitle
+					: layoutRes);
+			View back = view.findViewById(R.id.btn_back);
+			if (back == null) {
+				throw new IllegalArgumentException(
+						"can not find R.id.btn_back in customView");
+			}
+
+			back.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					TDevice.hideSoftKeyboard(getCurrentFocus());// 隐藏软键盘
+					onBackPressed();// 按下了返回键
+				}
+			});
+
+			mTvActionTitle = (TextView) view
+					.findViewById(R.id.tv_actionbar_title);
+
+			if (mTvActionTitle == null) {
+				throw new IllegalArgumentException(
+						"can not find R.id.tv_actionbar_title in customView");
+			}
+			int titleRes = getActionBarTitle();
+			if (titleRes != 0) {
+				mTvActionTitle.setText(titleRes);
+			}
+			LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+					LayoutParams.MATCH_PARENT);
+
+			// ------------------------------------------------- 创建自定义布局 ↑
+			// 设置自定义内容
+			actionBar.setCustomView(view, params);
+			View spinner = actionBar.getCustomView().findViewById(R.id.spinner);
+			if (haveSpinner()) {
+				spinner.setVisibility(View.VISIBLE);
+			} else {
+				spinner.setVisibility(View.GONE);
+			}
+
+		} else {
+			actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
+			actionBar.setDisplayUseLogoEnabled(false);
+			int titleRes = getActionBarTitle();
+			if (titleRes != 0) {
+				actionBar.setTitle(titleRes);
+			}
 		}
 	}
-	
+
+	protected Spinner getSpinner() {
+		return (Spinner) mActionBar.getCustomView().findViewById(R.id.spinner);
+	}
+
+	public void setActionBarTitle(int resId) {
+		if (resId != 0) {
+			setActionBarTitle(getString(resId));
+		}
+	}
+
+	public void setActionBarTitle(String title) {
+		if (StringUtils.isEmpty(title)) {
+			title = getString(R.string.app_name);
+		}
+		if (hasActionBar() && mActionBar != null) {
+			if (mTvActionTitle != null) {
+				mTvActionTitle.setText(title);
+			}
+			mActionBar.setTitle(title);
+		}
+	}
+
+	protected boolean haveSpinner() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	protected int getActionBarTitle() {
+		// TODO Auto-generated method stub
+		return R.string.app_name;
+	}
+
+	protected View inflateView(int resId) {
+		return mInflater.inflate(resId, null);
+	}
+
+	protected int getAtionBarCustomView() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	protected boolean hasBackButton() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	protected int getLayoutID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
+		TDevice.hideSoftKeyboard(getCurrentFocus());// 隐藏键盘
+		ButterKnife.reset(this);
 	}
-	
+
 	@Override
 	public void initView() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initData() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	protected boolean hasActionBar() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
+	protected void onBeforeSetContentLayout() {
+		// TODO Auto-generated method stub
+
+	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+    public void showToast(int msgResid, int icon, int gravity) {
+        showToast(getString(msgResid), icon, gravity);
+    }
+
+    public void showToast(String message, int icon, int gravity) {
+        CommonToast toast = new CommonToast(this);
+        toast.setMessage(message);
+        toast.setMessageIc(icon);
+        toast.setLayoutGravity(gravity);
+        toast.show();
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+
+        // setOverflowIconVisible(featureId, menu);
+        return super.onMenuOpened(featureId, menu);
+    }
+
+	protected int getLayoutId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/com/example/oschina_client/ui/SimpleBackActivity.java
+++ src/com/example/oschina_client/ui/SimpleBackActivity.java
@@ -1,11 +1,188 @@
 package com.example.oschina_client.ui;
 
+import java.lang.ref.WeakReference;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.oschina_client.R;
 import com.example.oschina_client.base.BaseActivity;
+import com.example.oschina_client.base.BaseFragment;
+import com.example.oschina_client.bean.SimpleBackPage;
+import com.example.oschina_client.emoji.OnSendClickListener;
 
-public class SimpleBackActivity extends BaseActivity {
+public class SimpleBackActivity extends BaseActivity implements
+		OnSendClickListener {
 
-	public static final String BUNDLE_KEY_ARGS = null;
-	public static final String BUNDLE_KEY_PAGE = null;
+	public static final String BUNDLE_KEY_ARGS = "BUNDLE_KEY_ARGS";
+	public static final String BUNDLE_KEY_PAGE = "BUNDLE_KEY_PAGE";
+	private static final String TAG = "FLAG_TAG";
+	protected int mPageValue = -1;
+
+	protected WeakReference<Fragment> mFragment;
+
+	@Override
+	protected int getLayoutId() {
+		return R.layout.activity_simple_fragment;
+	}
+
+	@Override
+	protected boolean hasBackButton() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void init(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.init(savedInstanceState);
+		if (mPageValue == -1) {
+			mPageValue = getIntent().getIntExtra(BUNDLE_KEY_PAGE, 0);
+		}
+		initFromIntent(mPageValue, getIntent());
+	}
+
+	protected void initFromIntent(int pageValue, Intent data) {
+		// TODO Auto-generated method stub
+		if (data == null) {
+			throw new RuntimeException(
+					"you must provide a page info to display");
+		}
+		SimpleBackPage page = SimpleBackPage.getPageByValue(pageValue);
+		if (page == null) {
+			throw new IllegalArgumentException("can not find page by value:"
+					+ pageValue);
+		}
+
+		setActionBarTitle(page.getTitle());
+
+		try {
+			Fragment fragment = (Fragment) page.getClz().newInstance();
+
+			Bundle args = data.getBundleExtra(BUNDLE_KEY_ARGS);
+
+			if (args != null) {
+				fragment.setArguments(args);
+			}
+
+			FragmentTransaction trans = getSupportFragmentManager()
+					.beginTransaction();
+			trans.replace(R.id.container, fragment, TAG);
+			trans.commitAllowingStateLoss();
+
+			mFragment = new WeakReference<Fragment>(fragment);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			throw new IllegalArgumentException(
+					"generate fragment error. by value:" + pageValue);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		// if (mFragment.get() instanceof TweetsFragment) {
+		// setActionBarTitle("话题");
+		// }
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		// case R.id.public_menu_send:
+		// if (mFragment.get() instanceof TweetsFragment) {
+		// sendTopic();
+		// } else {
+		// return super.onOptionsItemSelected(item);
+		// }
+		// break;
+		default:
+			break;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// if (mFragment.get() instanceof TweetsFragment) {
+		// getMenuInflater().inflate(R.menu.pub_topic_menu, menu);
+		// }
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	/**
+	 * 发送话题
+	 */
+	private void sendTopic() {
+		// Bundle bundle = new Bundle();
+		// bundle.putInt(TweetPubFragment.ACTION_TYPE,
+		// TweetPubFragment.ACTION_TYPE_TOPIC);
+		// bundle.putString("tweet_topic", "#"
+		// + ((TweetsFragment) mFragment.get()).getTopic() + "# ");
+		// UIHelper.showTweetActivity(this, SimpleBackPage.TWEET_PUB, bundle);
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		if (mFragment != null && mFragment.get() != null
+				&& mFragment.get() instanceof BaseFragment) {
+			BaseFragment bf = (BaseFragment) mFragment.get();
+			if (!bf.onBackPressed()) {
+				super.onBackPressed();
+			}
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.ACTION_DOWN
+				&& mFragment.get() instanceof BaseFragment) {
+			((BaseFragment) mFragment.get()).onBackPressed();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	protected void onActivityResult(int arg0, int arg1, Intent arg2) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(arg0, arg1, arg2);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		super.onClick(v);
+	}
 	
+    @Override
+    public void initView() {}
 
+    @Override
+    public void initData() {}
+    
+	@Override
+	public void onClickSendButton(Editable str) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClickFlagButton() {
+		// TODO Auto-generated method stub
+		
+	}
 }
